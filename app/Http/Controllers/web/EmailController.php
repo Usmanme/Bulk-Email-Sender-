85a2de83e007ea8f5043f5114fdd1c600999f207
MrNi8mare <zjaved.bese17seecs@seecs.edu.pk>
2023-10-24 13:16:31 +0500

    restored the document store function

--- app/Http/Controllers/web/EmailController.php
+++ app/Http/Controllers/web/EmailController.php
@@ -59,6 +59,44 @@ class EmailController extends Controller
     //     }
     // }
 
+    public function store(Request $request)
+    {
+        
+        // Validate the incoming request
+        $this->validate($request, [
+            'document_name.*' => 'required|mimes:pdf,doc,docx', // Example validation for file types and maximum file size
+        ]);
+
+        $files = [];
+     
+        if ($request->file('document_name')){
+            
+            foreach($request->file('document_name') as $key => $file)
+            {
+                
+                $fileName = $file->getClientOriginalName();  
+               
+                $file->move(public_path('uploads'), $fileName);
+                $files[]['name'] = $fileName;
+            }
+        }
+        
+        
+        foreach ($files as $key => $file) {
+           
+            $file_name = pathinfo($file['name'], PATHINFO_FILENAME); 
+            $document = new Document();
+            $document->document_path = json_encode($file['name']);
+            $document->document_name = json_encode($file_name);
+            $document->user_id = Auth::user()->id;
+            $document->save();
+        }
+        
+     
+
+        return back()->with('success', 'Files uploaded successfully.');
+    }
+
     public function importView()
     {
         $email_files = EmailFile::where('user_id', Auth::user()->id)->get();
