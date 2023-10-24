0afe6d6c2308996fd594449c81102f08976e0f9a
MrNi8mare <zjaved.bese17seecs@seecs.edu.pk>
2023-10-24 11:01:52 +0500

    import file controller re-add

--- app/Http/Controllers/web/EmailController.php
+++ app/Http/Controllers/web/EmailController.php
@@ -41,23 +41,23 @@ class EmailController extends Controller
         }
     }
 
-    public function send(Request $request)
-    {
+    // public function send(Request $request)
+    // {
         
-        // Validate the incoming request
-        $this->validate($request, [
-            'subject' => 'required|string',
-            'body' => 'required|string',
-        ]);
+    //     // Validate the incoming request
+    //     $this->validate($request, [
+    //         'subject' => 'required|string',
+    //         'body' => 'required|string',
+    //     ]);
 
-        try {
-            Mail::to('usman.islootech@gmail.com')->send(new SendMail($request->subject, $request->body, $request->name));
+    //     try {
+    //         Mail::to('usman.islootech@gmail.com')->send(new SendMail($request->subject, $request->body, $request->name));
 
-            return back()->withSuccess('Email Sent Successfully!..');
-        } catch (Exception $ex) {
-            return back()->withDanger('Something went wrong!' . ' ' . $ex->getMessage());
-        }
-    }
+    //         return back()->withSuccess('Email Sent Successfully!..');
+    //     } catch (Exception $ex) {
+    //         return back()->withDanger('Something went wrong!' . ' ' . $ex->getMessage());
+    //     }
+    // }
 
     public function importView()
     {
@@ -69,7 +69,27 @@ class EmailController extends Controller
 
     public function importFile(Request $request)
     {
-        dd("Here Done");
+        $this->validate($request, [
+            'file' => 'required',
+        ]);
+        $file = $request->file('file');
+        $extension = $file->getClientOriginalExtension();
+        $originalFileName = $file->getClientOriginalName();
+        $fileName = uniqid('document_') . '.' . $extension;
+        Storage::disk('public')->put('email_files/'.$fileName, file_get_contents($file));
+
+        $email_file = new EmailFile;
+        $email_file->user_id = Auth::user()->id;
+        $email_file->file_name = $fileName;
+        $email_file->file_extension = $extension;
+        $email_file->original_file_name = $originalFileName;
+        $email_file->save();
+
+        $file_id = $email_file->id;
+
+        event (new EmailFileImported($fileName, $file_id));
+        return redirect()->back()->with('success', 'Files uploaded successfully.');
+
     }
 
     public function history()
